0559ea07caed35715b8203c1e3abdd9c332bbf21
radovantrtil <radovan.trtil@gmail.com>
2023-02-23 11:39:01 +0100

    encrypted room - create and invite

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const sdk = require("matrix-js-sdk");
 
+const xtrtilId= "@xtrtil1:matrix.org";
 const myUserID = "@radovantrtil1:matrix.org";
 const myAccessToekn = "syt_cmFkb3ZhbnRydGlsMQ_nlgCLnwPbVhxcBOaTOln_0yS1SC"
 
-const testRoomID = "!ambjRjYWCPSvILAPze:matrix.org";
-//!jjrsLQPtuOWsvGelBL:matrix.org
+const testRoomID = "!KQANJEAzaiMyqMFVlA:matrix.org";
 
 const client = sdk.createClient({
     baseUrl: "https://matrix.org",
     accessToken: myAccessToekn,
     userId: myUserID
 });
-
-const recipient = "!cJujgkNfmdLISHmkoq:matrix.org";
 
 //client.publicRooms(function(err, data) {
 //      console.log("Public Rooms: %s", JSON.stringify(data));
@@ -32,17 +30,6 @@ client.on("RoomMember.membership", function(event, member) {
     }
 });
 
-
-// Listen for typing changes
-client.on("RoomMember.typing", function(event, member) {
-    if (member.typing) {
-        console.log(member.name + " is typing...");
-    }
-    else {
-        console.log(member.name + " stopped typing.");
-    }
-});
-
 client.on("Room.timeline", function(event, room, toStartOfTimeline) {
     if (toStartOfTimeline) {
         return; // don't print paginated results
@@ -54,10 +41,7 @@ client.on("Room.timeline", function(event, room, toStartOfTimeline) {
     console.log("(%s) %s :: %s", room.name, event.getSender(), event.getContent().body);
 });
 
-// start the client to setup the connection to the server
-client.startClient({initialSyncLimit: 10}).then((response)=> {
-    console.log(response)
-});
+
 
 client.once('sync', function(state, prevState, res) {
     if(state === 'PREPARED') {
@@ -69,24 +53,14 @@ client.once('sync', function(state, prevState, res) {
 });
 
 const content = {
-    "body": "Ahoj lidi!",
-    "msgtype": "m.text"
+    body: "Ahoj lidi!",
+    msgtype: "m.text"
 };
+
 client.sendEvent(testRoomID, "m.room.message", content, "", (err, res) => {
     console.log(err);
 });
 
-const mess = {
-    "body": "Ahoj člověče!",
-    "msgtype": "m.text"
-}
-
-client.sendEvent(recipient, "m.room.message", mess
-).then((response) => {
-    console.log(`Message sent to ${recipient}: ${mess.body}`);
-}).catch((error) => {
-    console.error(`Failed to send message to ${recipient}: ${error}`);
-});
 
 client.on("Room.timeline", function(event, room, toStartOfTimeline) {
     if (event.getType() !== "m.room.message") {
@@ -106,8 +80,47 @@ rooms.forEach(room => {
     });
 });
 
+client.createRoom({
+        name:"Tohle je mistnost",
+        preset:"private_chat",
+        visibility:"private",
+        initial_state:
+            [
+                {
+                    type:"m.room.guest_access",
+                    state_key:"",
+                    content:{
+                        guest_access:"can_join"
+                    }
+                },
+                {
+                    type:"m.room.encryption",
+                    state_key:"",
+                    content:
+                        {
+                            algorithm:"m.megolm.v1.aes-sha2"
+                        }
+                }
+            ]
+})
+    .then(room => {
+        console.log(`Created room ${room.room_id}`);
+        // Do something with the roomId, such as inviting users
+        client.invite(room.room_id, xtrtilId)
+            .then(() => console.log(`Invited ${xtrtilId} to ${room.room_id}`))
+            .catch(err => console.error(`Failed to invite ${userId} to ${room.room_id}`, err));
+
+    })
+    .catch(err => console.error(`Failed to create room`, err));
+
 Object.keys(client.store.rooms).forEach((roomId) => {
     client.getRoom(roomId).timeline.forEach(t => {
         console.log(t.event);
     });
+});
+
+
+// start the client to setup the connection to the server
+client.startClient({initialSyncLimit: 10}).then((response)=> {
+    console.log(response)
 });
--- index.js
+++ index.js
@@ -15,3 +15,26 @@ const ciphertext = senderDevice.encrypt(plaintext);
 
 console.log(`Original message: ${plaintext}`);
 console.log(`Encrypted message: ${ciphertext}`);
+
+
+olm.init();
+const alice = new olm.Account();
+const bob = new olm.Account();
+alice.create();
+bob.create();
+bob.generate_one_time_keys(1);
+
+const bobs_id_keys = JSON.parse(bob.identity_keys());
+const bobs_id_key = bobs_id_keys.curve25519;
+const bobs_ot_keys = JSON.parse(bob.one_time_keys());
+for (key in bobs_ot_keys.curve25519) {
+    var bobs_ot_key = bobs_ot_keys.curve25519[key];
+}
+
+alice_session = new Olm.Session();
+alice_session.create_outbound(alice, bobs_id_key, bobs_ot_key);
+alice_message = a_session.encrypt("Hello");
+
+bob_session.create_inbound(bob, bob_message);
+const plaintext = bob_session.decrypt(message_1.type, bob_message);
+bob.remove_one_time_keys(bob_session);
